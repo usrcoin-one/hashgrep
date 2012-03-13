3f3a6d130f14ea76441f1d75efcdc1f2b8a66b08
Bin Fan <binfan@cs.cmu.edu>
2012-03-13 00:08:23 -0400

    replace c++ style getline by c style read

--- cpu/hashgrep.cpp
+++ cpu/hashgrep.cpp
@@ -32,6 +32,7 @@ The code has been tested on a Linux 2.6.35 machine, gcc version 4.4.5.
 #include <sys/mman.h>
 #include <xmmintrin.h>
 #include <random>
+#include <vector>
 
 #include "hashgrep.h"
 
@@ -196,62 +197,175 @@ void Filter::buildFilter(char* phrasefilename) throw(FilterError)
 
 void Filter::processCorpus(int fd) throw(FilterError)
 {
-    const int size = 512 * 1024;
+    //const int size = 512 * 1024;
+    const int size = 64;
     char* buff = new char[size];
 
-    string line;
-    size_t total = 0;
-    size_t hit = 0;
-    while (getline(cin, line))
-    {
+    int crtLineSize = 4*1024;
+    char* line = new char[crtLineSize + 1];
+    line[0] = '\0';
 
-        //cout << "----" << line << endl;
-        bool printLine = false;
-        hash.reset();
-        const char* buff = line.data();
-        char* hashstart = (char* ) buff;
-        for (unsigned i = 0; i < line.length(); i++) {
-            updateHashes(buff[i]);
-            
-            if (hash.is_full()) {
-                //cout << "\t with " << i << "  \"" << hashstart << "\"" << endl;
-                total ++;
-                if (!checkInFilter()) {
-                    hashstart ++;
-                    continue;
+    size_t cnt0 = 0, cnt1 = 0, cnt2 = 0, cnt3 = 0;
+
+    hash.reset();
+    while(true) {
+        int r = read(fd, (void*)buff, size);
+        if (r < 0) {
+            perror("Error");
+            throw FilterError("Error while reading from file");
+        }
+
+        if (r == 0) {
+            break;
+        }
+
+        int lineStart = 0;
+        int hashStart = 0;
+        vector<int> vec1;
+        vector<char*> vec2;
+        for (int i = 0; i < r; i++) {
+            if (buff[i] == '\n') {
+                buff[i] = '\0';
+                char* pos;
+                if (line[0] != '\0') {
+                    // line is in use:
+                    cout << vec1.size() << endl;
+                    cout << "yay " << line << endl;
+                    strncat(line, buff + lineStart, i - lineStart + 1 );
+                    pos = line;
+                    cout << vec1.size() << endl;
+                }
+                else {
+                    pos = buff + lineStart;
                 }
 
-                hit ++; 
-                
-                //hashstart ++; continue;
-
-                char* p;
-                if (hashfilter.Get(hash.h, p) == Ok) {
-
+                cout << "=============" << endl;
+                cout << pos << endl;
+                cout << vec1.size() << endl;
+                // perform strcmp for all recorded
+                bool printLine = false;
+                for (int j = 0; j < vec1.size(); j ++ ) {
+                    int hashStart = vec1[j];
+                    char *p = vec2[j];
+                    cout << "let\'s see " << pos + hashStart<< endl;
+                    //cout << hashStart << endl;
                     while (p != NULL) {
                         char* q = p + sizeof(char*);
-                        //cout << "\t checking " << q << endl;
-                        if (strncmp(hashstart, q, strlen(q)) == 0) {
+                        cout << "\t checking " << q << endl;
+                        if (strncmp(pos + hashStart, q, strlen(q)) == 0) {
                             printLine = true;
+                            cout << "match" << cnt3 << " line=" << pos << ", pattern = " << q << endl;
+
                             break;
                         }
                         p = *(char**) p;
                     }
-
                     if (printLine) {
-                        cout << line << endl;
+                        cnt3 ++;
+                        cout << pos << endl;
                         break;
                     }
+                    
                 }
-                hashstart  ++;
-               
+                cout << vec1.size() << endl;
+                vec1.clear();
+                vec2.clear();
+                line[0] = '\0';
+
+                hash.reset();
+                lineStart = i + 1;
+                hashStart = 0;
+                continue;
             }
+          
+            updateHashes(buff[i]);
+
+            if (hash.is_full()) {
+                cnt0 ++;
+                if (checkInFilter()) {
+                    cnt1 ++;
+
+                    char* p;
+                    if (hashfilter.Get(hash.h, p) == Ok) {
+                        cnt2 ++;
+                        cout << "push_back\n" << buff + hashStart << endl ;
+                        vec1.push_back(hashStart);
+                        vec2.push_back(p);
+                        cout << vec1.size() << endl;
+                    } 
+                    
+                }
+                hashStart ++;
+            }
+        }
+
+        if (buff[r - 1] != '\n') {
+            if (r - lineStart > crtLineSize)
+            {
+                crtLineSize = (r - lineStart) * 2;
+                delete [] line;
+                line = new char[crtLineSize + 1];
+            }
+            memcpy(line, buff + lineStart, r - lineStart);
+            line[r - lineStart] = '\0';
+            cout << vec1.size() << endl;
         }
 
     }
 
-    cout << total << " " << hit << endl;
+    // string line;
+    // while (getline(cin, line))
+    // {
 
+    //     //cout << "----" << line << endl;
+    //     bool printLine = false;
+    //     hash.reset();
+    //     const char* buff = line.data();
+    //     char* hashstart = (char* ) buff;
+    //     for (unsigned i = 0; i < line.length(); i++) {
+    //         updateHashes(buff[i]);
+    //         total ++; continue;
+    //         if (hash.is_full()) {
+    //             //cout << "\t with " << i << "  \"" << hashstart << "\"" << endl;
+    //             total ++;
+    //             if (!checkInFilter()) {
+    //                 hashstart ++;
+    //                 continue;
+    //             }
+
+    //             hit ++; 
+                
+    //             //hashstart ++; continue;
+
+    //             char* p;
+    //             if (hashfilter.Get(hash.h, p) == Ok) {
+
+    //                 while (p != NULL) {
+    //                     char* q = p + sizeof(char*);
+    //                     //cout << "\t checking " << q << endl;
+    //                     if (strncmp(hashstart, q, strlen(q)) == 0) {
+    //                         printLine = true;
+    //                         break;
+    //                     }
+    //                     p = *(char**) p;
+    //                 }
+
+    //                 if (printLine) {
+    //                     cout << line << endl;
+    //                     break;
+    //                 }
+    //             }
+    //             hashstart  ++;
+               
+    //         }
+    //     }
+
+    // }
+
+    cerr << "Total " << cnt0 << " queries" << endl;
+    cerr << "Bloomfilter sees " << cnt1 << " hits, hitratio=" << 1.0 * cnt1/cnt0 << endl;
+    cerr << "Hashfilter sees " << cnt2 << " hits, hitratio=" << 1.0 * cnt2/cnt1 << endl;
+    cerr << "Strcmp sees " << cnt3 << " hits" << endl;
     delete [] buff;
 }
 
